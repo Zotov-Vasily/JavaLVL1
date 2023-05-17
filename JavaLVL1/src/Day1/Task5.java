2ab395fe7892da12d4835a078734eba4ed75c82e
Vasily Zotov <zotow.vasily90@gmail.com>
2023-05-17 21:15:25 +0200

    add Task5

---
+++ JavaLVL1/src/Day1/Task5.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        for(int year = 1980; year<2024;) {
+            System.out.println("Олимпиада " + year + " года");
+            year = year+4;
+        }
+
+    }
+}
