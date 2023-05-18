aeab5e10c7e718402bfa9547ef947fd8046afb68
Vasily Zotov <zotow.vasily90@gmail.com>
2023-05-18 19:50:26 +0200

    add Task6

---
+++ JavaLVL1/src/Day1/Task6.java
@@ -0,0 +1,17 @@
+package Day1;
+
+public class Task6 {
+    public static void main(String[] args){
+        int i=1;
+        int k=3;
+        int z=i*k;
+        while (i<10) {
+            System.out.println(i + "x" + k + "=" + z);
+            i++;
+
+
+        }
+
+    }
+}
+
