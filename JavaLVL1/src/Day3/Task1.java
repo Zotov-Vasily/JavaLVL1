fbccae95712d96afa040f21c41a190b3f1ce91d4
Vasily Zotov <zotow.vasily90@gmail.com>
2023-05-25 20:10:46 +0200

    add Task1

--- JavaLVL1/src/Day3/Task1.java
+++ JavaLVL1/src/Day3/Task1.java
@@ -1,7 +1,40 @@
 package Day3;
 
-public class Task1 {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите название города: ");
+        while (true) {
+            String cities = scan.nextLine();
+            if (cities.equalsIgnoreCase("Stop"))
+                break;
+
+            switch (cities) {
+                case "Москва":
+                case "Владивосток":
+                case "Ростов":
+                    System.out.println("Россия");
+                    break;
+                case "Рим":
+                case "Милан":
+                case "Турин":
+                    System.out.println("Италия");
+                    break;
+                case "Ливерпуль":
+                case "Манчестер":
+                case "Лондон":
+                    System.out.println("Англия");
+                    break;
+                case "Берлин":
+                case "Мюнхен":
+                case "Кёльн":
+                    System.out.println("Германия");
+                    break;
+                default:
+                    System.out.println("Неизвестная страна");
+            }
+        }
     }
 }
