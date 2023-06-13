761292670ce98e0be47711b4ecf19c13784c234a
Vasily Zotov <zotow.vasily90@gmail.com>
2023-06-13 19:21:43 +0200

    add Motorbike and Task2

---
+++ JavaLVL1/src/Day5/Task2.java
@@ -0,0 +1,9 @@
+package Day5;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2020, "ducati", "black");
+        System.out.println(motorbike.getModel() + "\n" + motorbike.getColor() + "\n" + motorbike.getYear());
+
+    }
+}
