2bf4e9df7badd56f9b5da45aca870ddd2b5a0da1
Vasily Zotov <zotow.vasily90@gmail.com>
2023-08-08 19:04:10 +0200

    Created a new list, filled it with even numbers from 0 to 30 and from 300 to 350. Outputted the list to the console.

---
+++ JavaLVL1/src/Day12/Task2.java
@@ -0,0 +1,18 @@
+package Day12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i <= 30; i += 2){
+            numbers.add(i);
+        }
+        for (int i = 300; i <= 350; i += 2){
+            numbers.add(i);
+        }
+        System.out.println(numbers);
+    }
+}
