07495a2c46023442c12457822c485513277ca40c
Vasily Zotov <zotow.vasily90@gmail.com>
2023-11-04 17:35:45 +0200

    Added a new “piece” to the ChessPiece enum, which will be called EMPTY. This “piece” will represent an empty space on the chessboard. Value of this figure is -1, and the string symbol is equal to the underscore ("_"). Then I created the ChessBoard class. This class, in its only constructor argument, takes as input a two-dimensional array of chess pieces. This two-dimensional array defines the configuration of the pieces on the chessboard. The empty space on the chessboard is specified using the EMPTY value. Also, the ChessBoard class implements the print() method, which displays the chessboard to the console. In the main() method of the Task2 class, I created a new object of the ChessBoard class, passing it as an argument a two-dimensional array with the arrangement of the pieces as in the picture (Kramnik - Kasparov match 2000). Then, using the print() method, I output the chessboard to the console, using string symbols for the chess pieces.

--- JavaLVL1/src/Day17/ChessPiece.java
+++ JavaLVL1/src/Day17/ChessPiece.java
@@ -4,7 +4,8 @@ public enum ChessPiece {
     KING_WHITE(100,"♔"), QUEEN_WHITE(9,"♕"), ROOK_WHITE(5,"♖"),
     BISHOP_WHITE(3.5,"♗"), KNIGHT_WHITE(3,"♘"), PAWN_WHITE(1,"♙"),
     KING_BLACK(100,"♚"), QUEEN_BLACK(9,"♛"), ROOK_BLACK(5,"♜"),
-    BISHOP_BLACK(3.5,"♝"), KNIGHT_BLACK(3,"♞"), PAWN_BLACK(1,"♟");
+    BISHOP_BLACK(3.5,"♝"), KNIGHT_BLACK(3,"♞"), PAWN_BLACK(1,"♟"),
+    EMPTY(-1,"_");
     private final double value;
     private final String symbol;
 
