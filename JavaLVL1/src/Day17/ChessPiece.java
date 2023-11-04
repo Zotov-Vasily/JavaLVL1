090be4bcee59c227df2f250f4b98685210d1604c
Vasily Zotov <zotow.vasily90@gmail.com>
2023-11-04 17:14:56 +0200

    Created Enum "ChessPiece". There are 6 different chess pieces in total: king, queen, rook, bishop, knight and pawn. In our program, each chess piece has the following characteristics: ● The value of the piece is a real number ● Line designation of a figure In our listing he created 12 figures - 6 white and 6 black. Each figure has two characteristics - its value and string designation. Using the created enum, in the main() method of the Task1 class, created an array, containing 4 white pawns and 4 black rooks. Output the contents of this array onto the screen using string symbols for chess pieces.

---
+++ JavaLVL1/src/Day17/ChessPiece.java
@@ -0,0 +1,21 @@
+package Day17;
+
+public enum ChessPiece {
+    KING_WHITE(100,"♔"), QUEEN_WHITE(9,"♕"), ROOK_WHITE(5,"♖"),
+    BISHOP_WHITE(3.5,"♗"), KNIGHT_WHITE(3,"♘"), PAWN_WHITE(1,"♙"),
+    KING_BLACK(100,"♚"), QUEEN_BLACK(9,"♛"), ROOK_BLACK(5,"♜"),
+    BISHOP_BLACK(3.5,"♝"), KNIGHT_BLACK(3,"♞"), PAWN_BLACK(1,"♟");
+    private final double value;
+    private final String symbol;
+
+    ChessPiece(double value, String symbol) {
+        this.value = value;
+        this.symbol = symbol;
+    }
+    public double getValue(){
+        return value;
+    }
+    public String getSymbol(){
+        return symbol;
+    }
+}
